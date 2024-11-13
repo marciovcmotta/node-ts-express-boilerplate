9a8c118f4729f9bb5f4f785f2bc5f804a21fe269
Marcio Motta <marciovcmotta@hotmail.com>
2024-11-13 16:11:18 -0300

    importing CORS

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import express from "express";
 import router from "./routers";
+import cors from "cors";
 
 function createApp(){
     const app = express();
+
+    const corsOption = {
+        origin: "*",
+        methods: ["GET", "POST", "PATCH", "DELETE"]
+    }
     
     app.use(express.json());
     app.use("/", router);
+    app.use(cors(corsOption));
 
     return app;
 }
